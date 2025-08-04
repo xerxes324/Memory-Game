75a6a5203bb166175bf0d3bdc1043fb4d9b4c876
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-04 23:51:17 +0530

    Retrieving individual pokemon data

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,4 +1,37 @@
-export default function Cards(){
+import { useEffect } from "react";
 
+function GeneratePokemon(props){
+    
+    // console.log(props.URLArray, "is the array");
+    useEffect( ()=>{
+
+        if ( props.URLArray.length !== 0){
+            // const pokemonArray = [];
+
+            const fetchFn = async(pokemon) => {
+                const req = await fetch(pokemon);
+                const data = await req.json();
+                console.log("the data is,", data);
+                
+            }
+    
+            for ( let i = 0 ; i < 10 ; i++){
+                fetchFn(props.URLArray[i]);
+            }
+        }
+    },[props.URLArray])
+}
+
+export default function Cards(props){
+    // console.log(props.URLArray,"is the CARDS array");
+
+    return(
+
+        <>
+            <div className="cards">
+                <GeneratePokemon URLArray = {props.URLArray}/>
+            </div>
+        </>
+    )
 }
 
