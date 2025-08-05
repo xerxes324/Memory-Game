8217e2074f5880cb10ea21b19cf291bb2ca0b192
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-05 16:53:03 +0530

    Temporary commit

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,25 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+function DisplayCards(props){
+    console.log(props.pokemonURL);
+}
+
 
 function GeneratePokemon(props){
     
     // console.log(props.URLArray, "is the array");
+    const [pokemon, setPokemon] = useState([]);
+
     useEffect( ()=>{
 
         if ( props.URLArray.length !== 0){
-            // const pokemonArray = [];
+            const pokemonArray = [];
 
             const fetchFn = async(pokemon) => {
                 const req = await fetch(pokemon);
                 const data = await req.json();
-                console.log("the data is,", data);
-                
+                // console.log("the data is,", data.sprites.front_default);
+                pokemonArray.push(data.sprites.front_default);
             }
     
             for ( let i = 0 ; i < 10 ; i++){
                 fetchFn(props.URLArray[i]);
             }
+
+            setPokemon(pokemonArray);
+            // console.log("the array is : ", pokemonArray);
         }
     },[props.URLArray])
+
+    return(
+        <>
+            <DisplayCards pokemonURL = {pokemon}/>
+        </>
+    )
 }
 
 export default function Cards(props){
